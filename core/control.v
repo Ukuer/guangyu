10795821ff3bdc3325c5d1992c020ab75a785872
Ukuer <2032108783@qq.com>
2020-09-29 15:24:02 +0800

    start control module

--- core/control.v
+++ core/control.v
@@ -3,17 +3,75 @@
 module control
 (
 	input [6:0]		opcode,
+	input [3:0]			shift_funct4,
 
+	output			shift_imm_sel,
+
+	output			ex_branch,
+	output[1:0]		ex_aluop,
+	output			ex_alu_imm_sel,
+	
 	output			wb_reg_write,
 	output			wb_memtoreg,
-	output			m_branch,
+
 	output			m_mem_read,
 	output			m_mem_write,
-	output			ex_aluop,
-	output			ex_alu_selimm,
-	output [1:0]	alu_op
 
 );
 
+reg [6:0] opcode;
 
+always @(*)
+begin
+	case(opcode):begin
+	
+	OPCODE_R_TYPE:	begin 
+		shift_imm_sel <= 1'b0;
+		ex_branch <= 1'b0;
+		ex_aluop <= 2'b10;
+		ex_alu_imm_sel <= 1'b0;
+		wb_reg_write <= 1'b1;
+		wb_memtoreg <= 1'b0;
+		m_mem_read <= 1'b0;
+		m_mem_write <= 1'b0;
+		end
+	OPCODE_IA_TYPE: begin
+		if (shift_funct4 == 4'b0001 ||
+			shift_funct4 == 4'b0101 ||
+			shift_funct4 == 4'b1101)
+			
+			shift_imm_sel <= 1'b1;
+		else 
+			shift_imm_sel <= 1'b0;
+
+		ex_branch <= 1'b0;
+		ex_aluop <= 2'b11;
+		ex_alu_imm_sel <= 1'b1;
+		wb_reg_write <= 1'b1;
+		wb_memtoreg <= 1'b0;
+		m_mem_read <= 1'b0;
+		m_mem_write <= 1'b0;
+		end
+		
+		OPCODE_ID_TYPE: begin 
+		shift_imm_sel <= 1'b0;
+		ex_branch <= 1'b0;
+		ex_aluop <= 2'b01;
+		ex_alu_imm_sel <= 1'b0;
+		wb_reg_write <= 1'b1;
+		wb_memtoreg <= 1'b1;
+		m_mem_read <= 1'b1;
+		m_mem_write <= 1'b0;
+		end
+		
+		OPCODE_B_TYPE: begin
+		shift_imm_sel <= 1'b0;
+		ex_branch <= 1'b1;
+		ex_aluop <= 2'b10;		// wait
+		ex_alu_imm_sel <= 1'b1;
+		wb_reg_write <= 1'b0;
+		wb_memtoreg <= 1'b0;
+		m_mem_read <= 1'b0;
+		m_mem_write <= 1'b0;
+		end
 
